64f3aec2b0ed35d387566be24c66e2dc4d4d88e0
Mark Mburu <markmburu685@gmail.com>
2023-04-03 18:38:00 +0300

    Formatted utils

--- src/main/java/com/markmburu/interview/urlshortener/utils/ConvertUtils.java
+++ src/main/java/com/markmburu/interview/urlshortener/utils/ConvertUtils.java
@@ -1,19 +1,9 @@
 package com.markmburu.interview.urlshortener.utils;
 
-/**
- * Provides encoding and decoding methods Base10 to Base62 and vice versa
- */
 public class ConvertUtils {
-    /**
-     * Randomized a-z A-Z 0-9 characters. Each character appears only once in the string
-     */
     public static final String ALPHABET = "Mheo9PI2qNs5Zpf80TBn7lmRbtQ4YKXHvwAEWxuzdra316OJigGLSVUCyFjkDc";
     public static final int BASE = ALPHABET.length();
 
-    /**
-     * @param num Base10 number of type Long
-     * @return base62 encoded string representation of input Long
-     */
     public static String idToStr(Long num) {
         StringBuilder str = new StringBuilder();
         while (num > 0) {
@@ -23,10 +13,6 @@ public class ConvertUtils {
         return str.toString();
     }
 
-    /**
-     * @param str Base62 encoded string
-     * @return decoded Base10 number of type Long
-     */
     public static Long strToId(String str) {
         Long num = 0L;
         for (int i = 0; i < str.length(); i++) {
--- src/main/java/com/markmburu/interview/urlshortener/utils/UrlUtil.java
+++ src/main/java/com/markmburu/interview/urlshortener/utils/UrlUtil.java
@@ -6,12 +6,6 @@ import java.net.URL;
 public class UrlUtil {
     private UrlUtil() {
     }
-
-    /**
-     * @param url any valid url
-     * @return returns protocol://domain:port/ part of the input url
-     * @throws MalformedURLException
-     */
     public static String getBaseUrl(String url) throws MalformedURLException {
         URL reqUrl = new URL(url);
         String protocol = reqUrl.getProtocol();
